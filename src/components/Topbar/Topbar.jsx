23749ec4caf205407a09b948b9d490377f7acf01
DESKTOP-GO7PEOM\DELL <fabriciolugao@gmail.com>
2025-03-07 18:09:57 -0300

    Alterando estilos do componentes TopBar para styled-components

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,15 +1,47 @@
 import React from "react";
-import "./Topbar.css";
+import styled from "styled-components";
+
+const TopBarContainer = styled.div`
+  background: rgb(111, 66, 193);
+  color: white;
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  z-index: 1000;
+`;
+
+const SiteName = styled.div`
+  font-size: 1.5em;
+  font-weight: bold;
+`;
+
+const Links = styled.a`
+  color: white;
+  text-decoration: none;
+  margin-left: 20px;
+  font-weight: 400;
+  transition: opacity 0.3s;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`;
 
 function Topbar() {
   return (
-    <div className="topbar">
-      <div className="site-name">PlayTube</div>
-      <div className="links">
-        <a href="#">Cadastrar</a>
-        <a href="#">Entrar</a>
+    <TopBarContainer>
+      <SiteName>PlayTube</SiteName>
+      <div>
+        <Links href="#">Cadastrar</Links>
+        <Links href="#">Entrar</Links>
       </div>
-    </div>
+    </TopBarContainer>
   );
 }
 
